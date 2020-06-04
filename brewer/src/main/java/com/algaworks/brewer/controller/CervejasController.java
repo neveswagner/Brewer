7fe15174737e9acb472c20edc8d0443c1c62e43a
neveswagner <wagnerneves.unip@outlook.com>
2020-06-04 20:02:56 -0300

    conexao com banco

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.repository.Cervejas;
 import com.algaworks.model.Cerveja;
 
 @Controller
 public class CervejasController {
 	
+	@Autowired
+	private Cervejas cervejas;
+	
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
-		return "cerveja/cadastroCerveja";
+		cervejas.findAll(); // Apagar...
+		
+		return "cerveja/CadastroCerveja";
 	}
 
 
